9e9a0adab4906be71e1a4796d0db3d500a60fb51
Thunyiwe Mkhize <mkhizept@dutappfactory.tech>
2022-05-26 17:42:55 +0200

    Races AppService done

---
+++ src/Dut.Get.Good.Application.Contracts/GetGoodApplicationContracts/Races/DTO/RaceDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Application.Dtos;
+
+namespace Dut.Get.Good.GetGoodApplicationContracts.Races.DTO
+{
+    public class RaceDto : EntityDto<Guid>
+    {
+        public string RaceDescription { get; set; }
+    }
+}
