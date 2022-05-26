6948f2a508c0de1cf9f4d9aa4a3cba8a624518a9
Ally Nsusa <allin@dutappfactory.tech>
2022-05-26 14:26:12 +0200

    removed unused references

--- src/Dut.Get.Good.Domain/GetGoodDomain/Entities/Attributes/Attribute.cs
+++ src/Dut.Get.Good.Domain/GetGoodDomain/Entities/Attributes/Attribute.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Volo.Abp.Domain.Entities;
 
 namespace Dut.Get.Good.GetGoodDomain.Entities.Attributes
