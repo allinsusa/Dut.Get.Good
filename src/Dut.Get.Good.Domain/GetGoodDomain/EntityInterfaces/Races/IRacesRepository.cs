928eb708d8ed784939f8e2fefc7e940e0b392c44
Thunyiwe Mkhize <mkhizept@dutappfactory.tech>
2022-05-26 16:24:02 +0200

    Race Repo done

---
+++ src/Dut.Get.Good.Domain/GetGoodDomain/EntityInterfaces/Races/IRacesRepository.cs
@@ -0,0 +1,17 @@
+﻿using Dut.Get.Good.GetGoodDomain.Entities.Races;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Dut.Get.Good.GetGoodDomain.EntityInterfaces.Races
+{
+    public interface IRacesRepository
+    {
+        Task<List<RacesBasicInfo>> GetAllRaces(CancellationToken cancellationToken = default);
+        Task AddNewRace(NewRace NewRace, CancellationToken cancellationToken = default);
+        Task<RacesBasicInfo> GetRaceById(Guid ClassAbilityId, CancellationToken cancellationToken = default);
+    }
+}
